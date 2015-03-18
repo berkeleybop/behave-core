588dea6971723a34a644becbbe4a2ce412aaea2c
SC <github.com@genkisugi.net>
2015-03-18 00:07:45 -0700

    changed names, import json no longer taking my local json first; also neede to clean out .pyc files to get clean try

--- tests/steps/use_step_libraries.py
+++ tests/steps/use_step_libraries.py
@@ -2,4 +2,4 @@
 #### Not really much here but to indroduce the step libraries.
 ####
 
-from behave_core import json_steps, resource_steps, page_steps
+from behave_core import json_data_steps, resource_steps, page_steps
